1e6d15e42bb7fbf89a5eca41596afe36b30c8ed3
T-Wesst <code.daddy17@gmail.com>
2018-11-06 23:03:11 -0800

    fix answer

--- learnyounode/babysteps.js
+++ learnyounode/babysteps.js
@@ -1,17 +1,12 @@
-// let sum = 0;
+let sum = 0;
 
-// for (let i = 2; i < process.argv.length; i++) {
-//     sum += process.argv[i];
-// }
+for (let i = 2; i < process.argv.length; i++) {
+    sum += Number(process.argv[i]);
+}
 
 
 // process.argv.slice(2).forEach(function(item) {
 //     sum += Number(item);
 // })
 
-
-// process.argv[2].forEach(function(item) {
-//     sum += Number(item);
-// })
-
-// console.log(sum);
+console.log(sum);
