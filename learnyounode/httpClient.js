2a8d001532bbe7ac0e65d83097c708a06cfe0825
T-Wesst <code.daddy17@gmail.com>
2018-11-06 23:53:16 -0800

    complete http client

---
+++ learnyounode/httpClient.js
@@ -0,0 +1,9 @@
+let http = require("http"),
+  url = process.argv[2];
+http.get(url, function(response) {
+  console.log(response);
+  response.setEncoding("utf8");
+  response.on("data", function(data) {
+    console.log(data);
+  });
+});
