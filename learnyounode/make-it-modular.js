52579178403720aad77c1e780fde0ba863d91619
T-Wesst <code.daddy17@gmail.com>
2018-11-06 22:48:09 -0800

    complete modular

---
+++ learnyounode/make-it-modular.js
@@ -0,0 +1,10 @@
+let myModule = require('./myModule'),
+    dirName = process.argv[2],
+    fileExt = process.argv[3];
+
+myModule(dirName, fileExt, function(err, list) {
+    if(err) return console.error('Error: ', err);
+    list.forEach(function(file) {
+        console.log(file);
+    })
+})
